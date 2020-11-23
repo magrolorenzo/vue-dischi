db6a9044df98598ea990fb45f9073b1c60895ed9
Lorenzo Magro <magrolorenzo@gmail.com>
2020-11-23 15:35:15 +0100

    Creazione file

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+// Fare una chiamata ajax per recuperare una decina di dischi musicali
+// e utilizzando vue, stampare a schermo una card per ogni album.
+
+// L'API da chiamare è: https://flynn.boolean.careers/exercises/api/array/music
+
+// BONUS 1: Creare una select con tutti i generi dei dischi.
+// In base al genere che viene selezionato nella select,
+// vengono visualizzati i cd corrispondenti.
+// N.B.: per fare questo punto non è necessario modificare l'array dei dati
+
+// BONUS 2: Ordinare i dischi per anno di uscita.
